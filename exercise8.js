155122ee457e70791616347abaff67fe7cd7618a
Iggy <amargos.ignasi@gmail.com>
2017-02-27 15:56:22 +0100

    Exercise 8

---
+++ exercise8.js
@@ -0,0 +1,15 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+var url = 'mongodb://localhost:27017/learnyoumongo'
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err
+  // console.log(`Succesfully connected to DB=${db.s.databaseName}`)
+  db.collection('parrots')
+    .find({'age': {$gt: parseInt(process.argv[2])}})
+    .count(function (err, documents) {
+      console.log(documents)
+    })
+  db.close()
+})
